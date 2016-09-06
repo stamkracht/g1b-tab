2afeba64394a7f25711b1dac9c8af8d58e706bb7
Gleb <git@gleb.nl>
2016-09-06 02:05:42 +0200

    Update element speed and position to make movements appear more random.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 chrome.omnibox.onInputChanged.addListener(function (q) {
   var el = document.createElement('span');
-  var offset = Math.floor(Math.random() * 50);
-  var duration = Math.floor(Math.random() * 10);
-  el.innerHTML = '<span style="right:'+offset+'vw; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
   document.body.appendChild(el);
+  var updateElement = function() {
+    var offset = Math.floor(Math.random() * 50);
+    var duration = Math.floor(Math.random() * 10);
+    console.log(q.slice(-1), offset, duration);
+    el.innerHTML = '<span style="right:'+offset+'vw; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
+    setTimeout(function () {
+      update();
+    }, duration * 1000);
+  };
+  updateElement();
 });
 
 chrome.omnibox.onInputEntered.addListener(function (q) {
