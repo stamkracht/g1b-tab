2ba9ffc4b33dfe175b5b9beddd04a9fd4ed50701
Gleb <git@gleb.nl>
2016-09-06 02:58:00 +0200

    Using different font-sizes adds to the illusion of depth.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ chrome.omnibox.onInputChanged.addListener(function (q) {
   var el = document.createElement('span');
   document.body.appendChild(el);
   var updateElement = function() {
+    var size = Math.round(Math.random() * 10 + 15);
     var offset = Math.floor(Math.random() * 50);
     var duration = Math.floor(Math.random() * 10);
-    el.innerHTML = '<span style="right:'+offset+'vw; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
+    el.innerHTML = '<span style="right:'+offset+'vw; font-size: '+size+'px; animation-duration:'+duration+'s">'+q.slice(-1)+'</span>';
     setTimeout(function () {
       updateElement();
     }, duration * 1000);
